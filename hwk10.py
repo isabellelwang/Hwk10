078d7b00fef88a0f55514cdc251b134611e140f3
DanielleJusto <danielleejusto@gmail.com>
2025-11-25 17:02:10 -0500

    Lumber Problem done

--- hwk10.py
+++ hwk10.py
@@ -5,7 +5,38 @@ from random import randint
 
 ### Part 1: Lumber Mill
 def lumberSelection(prices:list, n:int) -> float:
-	return 0.0
+    # Make an nxn matrix -> time : O(n), space: O(n^2)
+    m = [0] * (n+1)
+    for i in range(0,n+1):
+        row = [0] * (n+1)
+        m[i] = row
+
+    # Fill matrix -> time : O(n^2)
+    for i in range(1, n+1): 
+        row_value = prices[i-1]
+        for j in range(1, n+1):
+            if i > j:                   # If the row < column, carry value from directly above
+                m[i][j] =  m[i-1][j]
+            else:
+                if j % i == 0:          # If the column is divisible by the row, calculate price
+                    m[i][j] = (j/i) * row_value       
+                else:                   # If the column is not divisible by the row, determine price of remainder 
+                    m[i][j] = (j//i * row_value) + m[i][j//i]
+                        
+    max = m[n][n]                       # Current max is corner of grid
+    col = n-1                       
+    curr = m[n][col]
+    while col != 0:
+        total = 0
+        if n % col == 0:                # If the n is divisible by column, determine price by multiplying
+            total = curr * n/col
+        else:
+            total += curr + m[i][n-col] # Otherwise find remainder, determine price
+        if total > max:                 # If bigger than max replace
+            max = total
+        col-=1                          # Increment counters
+        curr = m[n][col]
+    return max
 
 ### Part 2: Cash Register
 def calcPermutations(val:int) -> None:
